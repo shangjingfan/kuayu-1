c7f1b649ac6e8843955431f92c13f02145c454fc
shangjingfan <876625909@qq.com>
2021-02-04 21:20:25 +0800

    CORS

--- hackers-com/public/hackers.js
+++ hackers-com/public/hackers.js
@@ -1,33 +1,36 @@
-// const request = new XMLHttpRequest()
-// request.open("GET", 'http://qq.com:9999/friends.json')
-// request.onreadystatechange = ()=>{
-//   if(request.readyState === 4 && request.status === 200){
-//     console.log(request.response)
-//   }
-// }
-// request.send()
 
-// 封装
-function jsonp(url){
-  return new Promise((resolve, reject)=>{
-    const random ='hackersJSONPCallbackName' + Math.random()
-    window[random] = data =>{
+
+const request = new XMLHttpRequest()
+request.open("GET", 'http://qq.com:9999/friends.json')
+request.onreadystatechange = () => {
+  if (request.readyState === 4 && request.status === 200) {
+    console.log(request.response)
+  }
+}
+request.send()
+
+
+// 封装jsonp
+/* function jsonp(url) {
+  return new Promise((resolve, reject) => {
+    const random = 'hackersJSONPCallbackName' + Math.random()
+    window[random] = data => {
       // console.log(data)
       resolve(data)
     }
     const script = document.createElement('script')
     script.src = `${url}?callback=${random}` //jsonp约定函数名叫做callback
-    script.onload = () =>{
+    script.onload = () => {
       script.remove()
     }
-    script.onerror = (err)=>{
+    script.onerror = (err) => {
       reject(err)
     }
     document.body.appendChild(script)
   })
 }
 
-jsonp('http://qq.com:9999/friends.js').then((data)=>{console.log(data)})
+jsonp('http://qq.com:9999/friends.js').then((data) => { console.log(data) }) */
 
 // JSONP
 /* window.xxx = (data)=>{ //第一版
